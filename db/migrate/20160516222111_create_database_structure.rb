297f489aa45e9968ddc0e8e4a2e58455cca39095
Hosam Shahin <hosamlshahin@gmail.com>
2016-05-16 19:05:40 -0400

    update migrations

--- db/migrate/20160516222111_create_database_structure.rb
+++ db/migrate/20160516222111_create_database_structure.rb
@@ -374,35 +374,11 @@ class CreateDatabaseStructure < ActiveRecord::Migration
         t.datetime "updated_at"
       end
 
-      create_table "users", force: :cascade do |t|
-        t.string   "email",                  limit: 255, default: "", null: false
-        t.string   "encrypted_password",     limit: 255, default: "", null: false
-        t.string   "reset_password_token",   limit: 255
-        t.datetime "reset_password_sent_at"
-        t.datetime "remember_created_at"
-        t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
-        t.datetime "current_sign_in_at"
-        t.datetime "last_sign_in_at"
-        t.string   "current_sign_in_ip",     limit: 255
-        t.string   "last_sign_in_ip",        limit: 255
-        t.string   "confirmation_token",     limit: 255
-        t.datetime "confirmed_at"
-        t.datetime "confirmation_sent_at"
-        t.datetime "created_at"
-        t.datetime "updated_at"
-        t.string   "first_name",             limit: 255
-        t.string   "last_name",              limit: 255
-        t.integer  "global_role_id",         limit: 4,                null: false
-        t.string   "avatar",                 limit: 255
-        t.string   "slug",                   limit: 255,              null: false
-        t.integer  "time_zone_id",           limit: 4
-      end
+      add_column :users, :global_role_id, :integer
+      add_column :users, :time_zone_id, :integer
 
-      add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-      add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
       add_index "users", ["global_role_id"], name: "index_users_on_global_role_id", using: :btree
       add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-      add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
       add_index "users", ["time_zone_id"], name: "index_users_on_time_zone_id", using: :btree
 
       add_foreign_key "course_enrollments", "course_offerings", name: "course_enrollments_course_offering_id_fk"
