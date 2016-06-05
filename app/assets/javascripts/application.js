294d4a220b575dc9c0ad4bac219e49d4922b4e58
Hosam Shahin <hosamlshahin@gmail.com>
2016-06-05 10:20:37 -0400

    revert back application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require modernizr.min.js
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require bootstrap-editable
 //= require bootstrap-editable-rails
 //= require js-routes
