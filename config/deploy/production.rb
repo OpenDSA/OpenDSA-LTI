750de56ccfc8c3a000d59c0b6094fac29950e7ef
Hosam Shahin <hosamlshahin@gmail.com>
2016-06-07 11:58:50 -0400

    change production server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,9 +6,9 @@ set :stage, :production
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@192.168.33.100}
-role :web, %w{deploy@192.168.33.100}
-role :db,  %w{deploy@192.168.33.100}
+role :app, %w{deploy@128.173.236.221}
+role :web, %w{deploy@128.173.236.221}
+role :db,  %w{deploy@128.173.236.221}
 
 
 # Extended Server Syntax
@@ -16,7 +16,7 @@ role :db,  %w{deploy@192.168.33.100}
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-server '192.168.33.100', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server '128.173.236.221', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
