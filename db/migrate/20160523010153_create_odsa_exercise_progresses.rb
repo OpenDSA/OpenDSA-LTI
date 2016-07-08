a5e1e36b050b26cafea09b9618f05c69d684aa9c
Hosam Shahin <hosamlshahin@gmail.com>
2016-07-08 10:46:24 -0400

    rename streak columns

--- db/migrate/20160523010153_create_odsa_exercise_progresses.rb
+++ db/migrate/20160523010153_create_odsa_exercise_progresses.rb
@@ -3,8 +3,8 @@ class CreateOdsaExerciseProgresses < ActiveRecord::Migration
     create_table :odsa_exercise_progresses do |t|
       t.integer  "user_id", null: false
       t.integer  "inst_book_section_exercise_id", null: false
-      t.integer  "current_score", null: false
-      t.integer  "highest_score", null: false
+      t.integer  "streak", null: false
+      t.integer  "longest_streak", null: false
       t.datetime "first_done", null: false
       t.datetime "last_done", null: false
       t.integer  "total_done", null: false
@@ -16,7 +16,5 @@ class CreateOdsaExerciseProgresses < ActiveRecord::Migration
       t.string "hinted_exercise"
       t.timestamps
     end
-
-    add_index :odsa_exercise_progresses, [:user_id, :inst_book_section_exercise_id], :unique => true, name: 'index_odsa_ex_prog_on_user_id_and_inst_bk_sec_ex_id'
   end
 end
---
+++ db/migrate/20160708141945_change_odsa_progress_column_name.rb
@@ -0,0 +1,9 @@
+class ChangeOdsaProgressColumnName < ActiveRecord::Migration
+  def change
+    rename_column :odsa_exercise_progresses, :streak, :current_score
+    rename_column :odsa_exercise_progresses, :longest_streak, :highest_score
+
+    add_index :odsa_exercise_progresses, [:user_id, :inst_book_section_exercise_id], :unique => true, name: 'index_odsa_ex_prog_on_user_id_and_inst_bk_sec_ex_id'
+
+  end
+end
