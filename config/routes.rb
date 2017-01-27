2e316209c065c8d168e77ae9519e9e83c44e23c9
Hosam Shahin <hosamlshahin@gmail.com>
2017-01-27 17:25:48 -0500

    test resource selection

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ CodeWorkout::Application.routes.draw do
   post 'lti/launch'
   post 'lti/assessment'
   get 'lti/xml_config', to: 'lti#xml_config', as: :xml_config
-  get 'lti/resource', to: 'lti#resource', as: :lti_resource
+  post 'lti/resource', to: 'lti#resource', as: :lti_resource
 
   resources :odsa_user_interactions
   resources :odsa_exercise_attempts
