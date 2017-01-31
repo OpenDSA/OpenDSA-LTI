621ed3e3e5f41d6eae5fb46a03883717a4407d05
Hosam Shahin <hosamlshahin@gmail.com>
2017-01-31 12:34:45 -0500

    Add copied! tooltip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -87,5 +87,32 @@ $.fn.editable.defaults.mode = 'inline';
 $(document).ready(function() {
     $('.xeditable').editable();
     var clip = new Clipboard('.clipboard-btn');
-    console.log(clip);
+
+    clip.on('success', function(e) {
+        setTooltip(e.trigger, 'Copied!');
+        hideTooltip(e.trigger);
+    });
+
+    clip.on('error', function(e) {
+        setTooltip(e.trigger, 'Failed!');
+        hideTooltip(e.trigger);
+    });
+
+    $('.clipboard-btn').tooltip({
+        trigger: 'click',
+        placement: 'bottom'
+    });
+
+    function setTooltip(btn, message) {
+        $(btn).tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+        setTimeout(function() {
+            $(btn).tooltip('hide');
+        }, 1000);
+    }
+
 });
