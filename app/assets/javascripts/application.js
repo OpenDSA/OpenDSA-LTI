c2b61bc1e043419dd9b0b2ebadeeea810b32e8a5
Hosam Shahin <hosamlshahin@gmail.com>
2017-03-06 13:48:55 -0500

    Add find_attempts.json.jbuilder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,7 +85,7 @@ function percentBar(filled, capacity, id) {
 $.fn.editable.defaults.mode = 'inline';
 
 $(document).ready(function() {
-    $('.xeditable').editable();
+    // $('.xeditable').editable();
     var clip = new Clipboard('.clipboard-btn');
 
     clip.on('success', function(e) {
