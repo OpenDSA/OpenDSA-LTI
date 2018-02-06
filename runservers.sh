2e3ae243fda13498443a6ffa5d7b3792fcc0fb30
Jackson Wonderly <jacksonwonderly@gmail.com>
2018-02-06 17:38:24 +0000

    update runservers.sh

--- runservers.sh
+++ runservers.sh
@@ -1,2 +1,13 @@
-cd /vagrant
-thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292
+# OpenDSA content server
+cd /vagrant/OpenDSA
+./WebServer &
+
+sleep 2
+# OpenDSA-LTI server
+cd /vagrant/OpenDSA-LTI
+bundle exec rake jobs:work &
+
+sleep 2
+# OpenDSA-LTI server
+cd /vagrant/OpenDSA-LTI
+bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292
