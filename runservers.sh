174338a9804422d7bb306a48c2e292f48fe78bf7
Weihao <weihao.gao@outlook.com>
2020-04-30 13:46:31 -0400

    update server script

--- runservers.sh
+++ runservers.sh
@@ -1,13 +1,16 @@
 # OpenDSA content server
 cd /vagrant/OpenDSA
-./WebServer &
+sudo lsof -t -i tcp:8080 | xargs kill -9
+(setsid ./WebServer &)
 
 sleep 2
 # OpenDSA-LTI server
 cd /vagrant/OpenDSA-LTI
-bundle exec rake jobs:work &
+sudo lsof -t -i tcp:9292 | xargs kill -9
+(setsid bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292 &)
 
 sleep 2
 # OpenDSA-LTI server
 cd /vagrant/OpenDSA-LTI
-bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292
+(setsid bundle exec rake jobs:work &)
+
