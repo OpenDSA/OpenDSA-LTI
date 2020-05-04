d7929db532d9ddf748ef68c9fcd905a0ab63f46c
Raghu1320 <raghu@vt.edu>
2020-05-04 15:12:23 -0400

    fixed syntax issue in pi attempts controller

--- app/controllers/pi_attempts_controller.rb
+++ app/controllers/pi_attempts_controller.rb
@@ -9,7 +9,7 @@ class PiAttemptsController < ApplicationController
   def get_attempts
     attempts = PiAttempt.where(user_id: current_user.id, frame_name: params[:frame_name], question: params[:question]).count
     result = {"result" => attempts}
-    respond_do |format|
+    respond_to do |format|
       format.html
       format.json {render json: result}
     end
@@ -19,7 +19,7 @@ class PiAttemptsController < ApplicationController
   def get_checkpoint
     checkpoint = PiAttempt.where(user_id: current_user.id, frame_name: params[:frame_name]).maximum("question")
     result = {"result" => checkpoint}
-    respond_do |format|
+    respond_to do |format|
       format.html
       format.json {render json: result}
     end
@@ -29,7 +29,7 @@ class PiAttemptsController < ApplicationController
   def get_progress
     progress = PiAttempt.where(user_id: current_user.id, frame_name: params[:frame_name], correct: 1).maximum("question")
     result = {"result" => progress}
-    respond_do |format|
+    respond_to do |format|
       format.html
       format.json {render json: result}
     end
