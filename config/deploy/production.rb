4fda303e7a7cb43a0e7c85e2fe307dce5116e6b9
awh4kc <awh4kc@virginia.edu>
2020-08-14 20:18:35 +0000

    Add production server details and update readme and Capfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :stage, :production
+et :stage, :production
 
 # Simple Role Syntax
 # ==================
@@ -6,9 +6,9 @@ set :stage, :production
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@128.173.236.80}
-role :web, %w{deploy@128.173.236.80}
-role :db,  %w{deploy@128.173.236.80}
+role :app, %w{deploy@192.168.86.224}
+role :web, %w{deploy@192.168.86.224}
+role :db,  %w{deploy@192.168.86.224}
 
 
 # Extended Server Syntax
@@ -16,7 +16,7 @@ role :db,  %w{deploy@128.173.236.80}
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-server '128.173.236.80', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server '192.168.86.224', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
