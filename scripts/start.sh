a7eabb4e140247062d1e2c8fa0884dd425789faf
Alex Hicks <awh4kc@vt.edu>
2020-09-10 23:38:46 -0400

    published logging

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,20 +21,20 @@ cp /opendsa-lti/config/databasedemo.yml /opendsa-lti/config/database.yml
 echo "-------------------------------------------------------"
 
 echo "-------------------------------------------------------"
-echo "updating permissions" >> ${OPENDSA_LOG_FILE} 2>&1
+echo "updating permissions" #>> ${OPENDSA_LOG_FILE} 2>&1
 ln -s /opendsa /opendsa-lti/public/OpenDSA || ERROR_FOUND=true
 if [[ "${ERROR_FOUND}" == true ]]; then cp -r /opendsa /opendsa-lti/public/OpenDSA && ERROR_FOUND=false; fi;
 echo "-------------------------------------------------------"
 
 cd "${OPENDSA_DIR}"
 echo "-------------------------------------------------------"
-echo "make -f Makefile pull" >> ${OPENDSA_LOG_FILE} 2>&1
-make -f Makefile pull >> ${OPENDSA_LOG_FILE} 2>&1
+echo "make -f Makefile pull" #>> ${OPENDSA_LOG_FILE} 2>&1
+make -f Makefile pull #>> ${OPENDSA_LOG_FILE} 2>&1
 echo "-------------------------------------------------------"
 
 echo "-------------------------------------------------------"
-echo "make -f Makefile venv" >> ${OPENDSA_LOG_FILE} 2>&1
-make -f Makefile venv >> ${OPENDSA_LOG_FILE} 2>&1
+echo "make -f Makefile venv" #>> ${OPENDSA_LOG_FILE} 2>&1
+make -f Makefile venv #>> ${OPENDSA_LOG_FILE} 2>&1
 sudo mkdir -p /home/deploy/OpenDSA
 sudo cp -r .pyVenv/ /home/deploy/OpenDSA/
 . .pyVenv/bin/activate
@@ -44,12 +44,12 @@ echo "cd /opendsa-lti" >> /root/.bashrc
 echo "-------------------------------------------------------"
 
 echo "-------------------------------------------------------"
-echo "pip install -r requirements.txt --ignore-installed" >> ${OPENDSA_LOG_FILE} 2>&1
-pip install -r requirements.txt --ignore-installed >> ${OPENDSA_LOG_FILE} 2>&1
+echo "pip install -r requirements.txt --ignore-installed" #>> ${OPENDSA_LOG_FILE} 2>&1
+pip install -r requirements.txt --ignore-installed #>> ${OPENDSA_LOG_FILE} 2>&1
 
 echo "-------------------------------------------------------"
-echo "make allbooks" >> ${OPENDSA_LOG_FILE} 2>&1
-make allbooks >> ${OPENDSA_LOG_FILE} 2>&1
+echo "make allbooks" #>> ${OPENDSA_LOG_FILE} 2>&1
+make allbooks #>> ${OPENDSA_LOG_FILE} 2>&1
 echo "-------------------------------------------------------"
 
 echo "Copying configuration files"
@@ -65,17 +65,17 @@ echo "-------------------------------------------------------"
 
 echo "Setting up database"
 echo "-------------------------------------------------------"
-echo "RAILS_ENV=$RAILS_ENV bundle exec db:schema:load" >> ${APP_LOG_FILE} 2>&1
-RAILS_ENV=${ENVIRONMENT} bundle exec rake db:schema:load >> ${APP_LOG_FILE} 2>&1
-echo "RAILS_ENV=$RAILS_ENV bundle exec db:seed" >> ${APP_LOG_FILE} 2>&1
-RAILS_ENV=${ENVIRONMENT} bundle exec rake db:seed >> ${APP_LOG_FILE} 2>&1
-echo "RAILS_ENV=$RAILS_ENV bundle exec db:populate" >> ${APP_LOG_FILE} 2>&1
-RAILS_ENV=${ENVIRONMENT} bundle exec rake db:populate >> ${APP_LOG_FILE} 2>&1
+echo "RAILS_ENV=$RAILS_ENV bundle exec db:schema:load" #>> ${APP_LOG_FILE} 2>&1
+RAILS_ENV=${ENVIRONMENT} bundle exec rake db:schema:load #>> ${APP_LOG_FILE} 2>&1
+echo "RAILS_ENV=$RAILS_ENV bundle exec db:seed" #>> ${APP_LOG_FILE} 2>&1
+RAILS_ENV=${ENVIRONMENT} bundle exec rake db:seed #>> ${APP_LOG_FILE} 2>&1
+echo "RAILS_ENV=$RAILS_ENV bundle exec db:populate" #>> ${APP_LOG_FILE} 2>&1
+RAILS_ENV=${ENVIRONMENT} bundle exec rake db:populate #>> ${APP_LOG_FILE} 2>&1
 echo "-------------------------------------------------------"
 
 echo "Starting server"
 echo "-------------------------------------------------------"
-echo "RAILS_ENV=$RAILS_ENV bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT}" >> ${APP_LOG_FILE} 2>&1
+echo "RAILS_ENV=$RAILS_ENV bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT}" #>> ${APP_LOG_FILE} 2>&1
 echo "RAILS_ENV=$RAILS_ENV bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT}"
-RAILS_ENV=${ENVIRONMENT} bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT} >> ${APP_LOG_FILE} 2>&1
+RAILS_ENV=${ENVIRONMENT} bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT} #>> ${APP_LOG_FILE} 2>&1
 echo "-------------------------------------------------------"
