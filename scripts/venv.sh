a3aa749ac6194c2165bef19e6c5e8d38b5fdda6b
Alex Hicks <awh4kc@virginia.edu>
2020-09-28 00:21:25 -0400

    update venv
    Signed-off-by: Alex Hicks <awh4kc@virginia.edu>

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,5 +1,7 @@
-make -f Makefile clean-venv venv
+make -f Makefile venv
 mkdir -p /home/deploy/OpenDSA
+ls
+ls .pyVenv
 cp -r .pyVenv/ /home/deploy/OpenDSA/
 . .pyVenv/bin/activate
 echo "cd /home/deploy/OpenDSA" >> /root/.bashrc
