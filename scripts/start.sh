15f2c97f99c0ba5556224b1a7a8ff1c699736179
Alex Hicks <awh4kc@virginia.edu>
2021-04-14 18:32:04 -0400

    switch to puma for devstack server
    Signed-off-by: Alex Hicks <awh4kc@virginia.edu>

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,6 +21,9 @@ echo "-------------------------------------------------------"
 echo "Create database.yml"
 cp /opendsa-lti/config/database.yml.example /opendsa-lti/config/database.yml
 echo "-------------------------------------------------------"
+echo "Create application.yml"
+cp /opendsa-lti/config/application.yml.example /opendsa-lti/config/application.yml
+echo "-------------------------------------------------------"
 echo "updating permissions" #>> ${OPENDSA_LOG_FILE} 2>&1
 rm -rf /opendsa/lti/public/OpenDSA
 ln -s /opendsa /opendsa-lti/public/OpenDSA
@@ -35,8 +38,6 @@ nohup bash -c "rake jobs:work >> ${APP_LOG_FILE} 2>&1 &"
 echo "-------------------------------------------------------"
 echo "Starting server"
 echo "-------------------------------------------------------"
-#echo "RAILS_ENV=$RAILS_ENV bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT}"
-echo "RAILS_ENV=$RAILS_ENV bundle exec thin start -p ${PORT}"
-#RAILS_ENV=${ENVIRONMENT} bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p ${PORT} #>> ${APP_LOG_FILE} 2>&1
-RAILS_ENV=${ENVIRONMENT} bundle exec thin start -p ${PORT} #>> ${APP_LOG_FILE} 2>&1
+echo "RAILS_ENV=${ENVIRONMENT} bundle exec puma -C config/puma-dev.rb"
+RAILS_ENV=${ENVIRONMENT} bundle exec puma -C config/puma-dev.rb
 echo "-------------------------------------------------------"
