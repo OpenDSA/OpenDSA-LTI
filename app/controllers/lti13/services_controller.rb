2befd9037bea4df36bc402747d951bd70bfd78f3
hshahin <hshahin@vt.edu>
2021-04-21 12:11:34 -0400

    remove call to verify_authenticity_token from services controller

--- app/controllers/lti13/services_controller.rb
+++ app/controllers/lti13/services_controller.rb
@@ -1,6 +1,4 @@
 class Lti13::ServicesController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   # POST /send_score?launch_id=1&access_token=ABC
   def send_score
     launch = Launch.find_by_id(params[:launch_id])
