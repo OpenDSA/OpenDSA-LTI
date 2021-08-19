1ed658946fef913e025e3109ec2d1b8eb64843ea
Alex Hicks <awh4kc@virginia.edu>
2021-08-19 00:15:36 -0400

    update env var and server names
    Signed-off-by: Alex Hicks <awh4kc@virginia.edu>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 OpenDSA::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "opendsa-staging.cs.vt.edu"
+  config.hosts << "opendsa-server.cs.vt.edu"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
