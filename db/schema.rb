458e430dd3e5f2e2ad0764a2189c3fd1063f1388
Bob Edmison <bedmison@gmail.com>
2021-12-21 16:34:13 -0500

    Updated version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_071954) do
+ActiveRecord::Schema.define(version: 2021_11_03_065052) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -407,6 +407,7 @@ ActiveRecord::Schema.define(version: 2021_04_18_071954) do
     t.bigint "inst_module_section_exercise_id"
     t.string "answer"
     t.integer "question_id"
+    t.boolean "finished_frame"
     t.index ["inst_book_id"], name: "odsa_exercise_attempts_inst_book_id_fk"
     t.index ["inst_book_section_exercise_id"], name: "odsa_exercise_attempts_inst_book_section_exercise_id_fk"
     t.index ["inst_course_offering_exercise_id"], name: "odsa_exercise_attempts_inst_course_offering_exercise_id_fk"
